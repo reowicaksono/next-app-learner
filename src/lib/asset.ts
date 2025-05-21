7087eda24d5dcbdf61069d3b80d57460b02696f8
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-05-21 07:35:51 +0700

    [add] icon home

--- src/lib/asset.ts
+++ src/lib/asset.ts
@@ -1,4 +1,11 @@
 import logo from "@img/logo.svg"
+import github from "@img/github.svg"
+import linkedin from "@img/linkedin.svg"
+import instagram from "@img/instagram.svg"
+
 export const LandingPage = {
-    logo: logo
+    logo: logo,
+    github: github,
+    linkedin: linkedin,
+    instagram: instagram,
 }
